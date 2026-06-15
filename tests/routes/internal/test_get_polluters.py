f34b9e8149372264103b175ff076d52fef12658d
kitenac <alabin500@gmail.com>
2026-06-15 03:12:44 +0300

    Devided schemas for each table into: create, modify and read versions and used them in corresponding endpoints request parametrs. Updated PUT methods to modify arbitry set of poles - just one or multiple at once from allowed ones (see schema_XXX_modify). Also added return type for each endpoint to prettify swager. PS: CI will be working after moving to sqlite for test mode

--- tests/routes/internal/test_get_polluters.py
+++ tests/routes/internal/test_get_polluters.py
@@ -2,5 +2,5 @@ def test_get_polluters(client):
     response = client.get("/polluter")
     
     assert response.status_code == 200
-    data = response.json()
-    assert isinstance(data, dict)   
+    resp = response.json()
+    assert isinstance(resp['data'], dict)   
